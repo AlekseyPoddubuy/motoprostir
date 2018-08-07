f75e6688a48af82514dbe7d38cc0c411ec3cb99a
Aleksey Poddubnuy <aprduction@gmail.com>
2018-08-07 17:04:27 +0300

    Add app mailer config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+	default from: 'from@example.com'
+	layout 'mailer'
+
+	def default_url_options
+		Rails.application.config.action_mailer.default_url_options
+	end
+
 end
